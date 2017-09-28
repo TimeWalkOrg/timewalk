4ae6725499ee562b08edadd30e8a6e3cc72d25a3
Michael Mayfield <mmfield@gmail.com>
2017-09-27 22:46:26 -0700

    Initial implementation of UI, game manager, and loader (#2)
    * Location and help commands, but slider WIP
    
    * Finally finished dynamic level label positioning.
    
    * Added change year
    
    * Added game time, lighting with flare, and restart/quite commands.
    
    * Day vs night state and toggling via UI (don't do anything yet)
    
    * Now changing night/day.
    
    * Pause and help text logic
    
    * Make sure skyboxes are assigned in env prefab
    
    * Project version back to 5.6.3

--- Assets/Platform/Scripts/TWGameManager.cs
+++ Assets/Platform/Scripts/TWGameManager.cs
@@ -5,45 +5,188 @@ using UnityEngine;
 namespace TimeWalk.Platform {
     using System;
     using UnityEngine.UI;
+    using UnityEngine.SceneManagement;
 
     public class TWGameManager : MonoBehaviour
     {
         public static TWGameManager instance = null;
-        
-        public TWLocationInfo timeWalkLocationInfo = null;
 
-        public List<TWLevel> timeWalkLevels = null;
+		public event Action TWLocationInfoChanged;
+		public event Action TWLevelsChanged;
+		public event Action TWLevelChanged;
+        public event Action TWNightDayChanged;
 
-        public TWLevel currentLevel = null; 
+		public static float startTimeHours = 12.0f; // noon
+		public static float timeSpeedUp = 12.0f; // 1 hour = 12 hours
 
-        public DateTime levelStartTime;
+        private static float secondsToHours = 1f / 3600f;
+		private TWLocationInfo timeWalkLocationInfo = null;
+        private List<TWLevel> timeWalkLevels = null;
+        private TWLevel currentLevel = null;
+        private float currentTimeHours = 0.0f;
+        private Boolean isDayTime = true;
+        private Boolean isPaused = false;
 
 
-        public event Action<TWLocationInfo> TWLocationInfoChanged;
+		public List<TWLevel> TimeWalkLevels
+		{
+			get
+			{
+				return timeWalkLevels;
+			}
+		}
+
+        public TWLevel CurrentLevel
+        {
+            get
+            {
+                return currentLevel;
+            }
+        }
+
+        //<summary>Current time in hours</summary>
+		public float CurrentTimeHours
+		{
+			get
+			{
+				return currentTimeHours;
+			}
+		}
+
+        public Boolean IsDayTime
+        {
+            get
+            {
+                return isDayTime;    
+            }
+        }
+
+        public Boolean IsPaused
+        {
+            get
+            {
+                return isPaused;
+            }
+        }
+
+		public TWLocationInfo TimeWalkLocationInfo
+		{
+			get
+			{
+				return timeWalkLocationInfo;
+			}
+		}
+
+        public Boolean DataReady()
+        {
+            return (timeWalkLevels != null &&
+                    currentLevel != null &&
+                    timeWalkLocationInfo != null);
+        }
+
+        public TWLevel GetLevelByYear(int year)
+        {
+            TWLevel level = null;
+            timeWalkLevels.ForEach(delegate (TWLevel l)
+            {
+                if(l.year == year) {
+                    level = l;
+                }
+            });
+            return level;
+        }
         
         public void OnLocationInfoChanged(TWLocationInfo locationInfo)
         {
+            if (locationInfo == null) return;
+
             if(TWLocationInfoChanged != null)
             {
                 timeWalkLocationInfo = locationInfo;
-                TWLocationInfoChanged(locationInfo);
+                startTimeHours = locationInfo.startTimeHours;
+                timeSpeedUp = locationInfo.timeSpeedUp;
+                TWLocationInfoChanged();
             }
         }
 
-        public event Action<List<TWLevel>, TWLevel> TWLevelsChanged;
-
-        public void OnTimeWalkLevelsChanged(List<TWLevel> levels, TWLevel current)
+        public void OnTimeWalkLevelsChanged(List<TWLevel> levels)
         {
-            if (TWLevelsChanged != null)
+            if (levels == null) return;
+
+			timeWalkLevels = levels;
+
+            timeWalkLevels.Sort();
+
+			if (TWLevelsChanged != null)
+			{
+				TWLevelsChanged();
+			}
+
+			SetCurrentLevel();
+        }
+
+        public void OnTimeWalkLevelChanged(TWLevel newLevel)
+		{
+            if (newLevel == null) return;
+
+			if (TWLevelChanged != null)
             {
-                levelStartTime = DateTime.Now;
-                timeWalkLevels = levels;
-                currentLevel = current;
-                TWLevelsChanged(timeWalkLevels, currentLevel);
+				currentLevel = newLevel;
+				TWLevelChanged();
+			}
+		}
+
+		public void OnTimeWalkLevelChanged()
+		{
+            int newLevelIdx = -1;
+            for (int i = 0; i < timeWalkLevels.Count; i++)
+            {
+                if(timeWalkLevels[i].year == currentLevel.year)
+                {
+                    newLevelIdx = (i == 0) ? 
+                        timeWalkLevels.Count - 1 : i - 1;
+                    break;
+                }
+            }
+
+            if (newLevelIdx > -1)
+            {
+                OnTimeWalkLevelChanged(timeWalkLevels[newLevelIdx]);
+            }
+		}
+
+        public void OnNightDayToggled()
+        {
+            // day == 6am-8pm
+            if (isDayTime) {
+                // Switch to night (8pm)
+                currentTimeHours = 20f + (currentTimeHours % 1);
+            }
+            else
+            {
+                // Switch to day (10am)
+				currentTimeHours = 10f + (currentTimeHours % 1);
             }
         }
 
-        void Awake()
+        public void OnRestart()
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+
+		public void OnQuit()
+		{
+			Application.Quit();
+		}
+
+        public void OnPause(Boolean pause)
+        {
+            Time.timeScale = pause ? 0 : 1;
+            isPaused = pause;
+        }
+
+
+		void Awake()
         {
             // Singleton
             if (instance == null)
@@ -58,14 +201,75 @@ namespace TimeWalk.Platform {
 
             // Don't destroy this object when new scene is loaded 
             DontDestroyOnLoad(gameObject);
+
+            // Set start time from location info
+            currentTimeHours = startTimeHours;
+        }
+
+        void Update()
+        {
+            currentTimeHours += Time.deltaTime * timeSpeedUp * secondsToHours;
+            if (currentTimeHours > 24.0f)
+            {
+                currentTimeHours %= 24.0f;
+            }
+
+            Boolean oldIsDayTime = isDayTime;
+
+            if(currentTimeHours >= 6f && currentTimeHours < 20f)
+            {
+                isDayTime = true;
+            } 
+            else
+            {
+                isDayTime = false;
+            }
+
+            if(oldIsDayTime != isDayTime) {
+                TWNightDayChanged();
+            }
+        }
+
+        private void SetCurrentLevel()
+        {
+			// Set current if null
+			if (currentLevel == null)
+			{
+				timeWalkLevels.ForEach(delegate (TWLevel l)
+				{
+					if (l.isDefault)
+					{
+						currentLevel = l;
+					}
+				});
+
+				// Just pick first level if no defaults
+				if (currentLevel == null)
+				{
+					currentLevel = timeWalkLevels[0];
+				}
+
+                TWLevelChanged();
+			}
         }
     }
 
     [System.Serializable]
-    public class TWLevel
+    public class TWLevel : IComparable
     {
         public int year;
         public string label;
+        public Boolean isDefault;
+
+        public int CompareTo(object obj)
+        {
+            if (obj == null) return 1;
+            TWLevel otherLevel = obj as TWLevel;
+			if (otherLevel != null)
+				return -1 * this.year.CompareTo(otherLevel.year);
+			else
+				throw new ArgumentException("Object is not a TWLevel");
+        }
     }
 
     [System.Serializable]
@@ -74,8 +278,10 @@ namespace TimeWalk.Platform {
         // Will need to rethink city and state for international locations
         public string city;
         public string state;
-        public Boolean isNight;
-        public Boolean isColor;
+        public Boolean isNight = false;
+        public Boolean isColor = true;
+        public float startTimeHours = TWGameManager.startTimeHours;
+        public float timeSpeedUp = TWGameManager.timeSpeedUp;
     }
 }
 
